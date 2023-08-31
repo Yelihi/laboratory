3aa73cc06ec260c542a057e197fc4d8720982c21
Yelihi <Yelihi19@gmail.com>
2023-09-01 00:32:02 +0900

    feat: Next.js practice dynamic route

--- 5. nextjs-test/nextjs-blog/lib/post.js
+++ 5. nextjs-test/nextjs-blog/lib/post.js
@@ -33,3 +33,26 @@ export function getSortedPostsData() {
     }
   });
 }
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  const matterResult = matter(fileContents);
+  return {
+    id,
+    ...matterResult.data,
+  };
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
--- 5. nextjs-test/nextjs-blog/pages/_app.js
+++ 5. nextjs-test/nextjs-blog/pages/_app.js
@@ -1,6 +1,7 @@
 // top level React component that wraps all pages in application
 // and setting globals.css
 import "../styles/globals.css";
+import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
